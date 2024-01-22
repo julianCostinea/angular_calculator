06c7171e894c9fcbad0f89b7d5d44061ec40a221
Emil Julian Costinea <ejc@arp-hansen.dk>
2024-01-22 14:54:43 +0100

    can now handle multiple operations;

--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -8,10 +8,10 @@ export class CalculatorService {
   calculatorResult = 0;
   pressedEquals = false;
   pressedOperator = false;
-  numberToOperate = '';
-  resultSoFar = 0;
-  operationForEquals = '';
-  firstOperation = true;
+  pressedNumber = false;
+  arrayToCalculate: Array<string> = [];
+  currentNumber = '';
+  currentOperator = '';
 
   addNumberToCurrentCalculation(key?: string) {
     if (!key) {
@@ -22,35 +22,34 @@ export class CalculatorService {
       alert('Too many keys');
       return;
     }
-
-    if (key === ',') {
-      if (this.pressedEquals) {
-        this.handleEqualsPressed();
-      }
-      //if there is already a comma in the string, dont add another
-      if (this.currentCalculation.includes(',') && !this.pressedOperator) {
-        return;
-      }
-      this.currentCalculation += key;
-      this.pressedOperator = false;
-      this.numberToOperate += key;
-      return;
-    }
-
-    const addedKey = parseInt(key, 10);
-
-    //shouldnt reach this but just in case
-    if (isNaN(addedKey)) {
-      alert('Not a number');
-      return;
-    }
-
-    //if equals was pressed, reset the calculation
     if (this.pressedEquals) {
       this.handleEqualsPressed();
     }
-    this.currentCalculation += addedKey;
-    this.numberToOperate += addedKey;
+
+    if (key === ',') {
+      //if there is already a comma in the string, dont add another
+      if (
+        this.currentCalculation.includes(',') &&
+        this.currentOperator === ''
+      ) {
+        return;
+      }
+      this.addNumberToCalculation(key);
+      return;
+    }
+
+    this.addNumberToCalculation(key);
+    if (this.currentOperator !== '') {
+      this.arrayToCalculate.push(this.currentOperator);
+      this.currentOperator = '';
+    }
+  }
+
+  addNumberToCalculation(key?: string) {
+    this.currentCalculation += key;
+    this.pressedOperator = false;
+    this.currentNumber += key;
+    this.pressedNumber = true;
   }
 
   addOperatorToCurrentCalculation(operator?: string) {
@@ -63,52 +62,37 @@ export class CalculatorService {
       return;
     }
 
-    //only allow one operator at a time, so we dont dwell into the loops
-    if (this.pressedOperator && !this.isOperatorCOrEquals(operator)) {
-      return;
+    const isEqualsOrClear = this.isOperatorCOrEquals(operator);
+
+    //if operator pressed replace the last operator with the new one
+    if (this.pressedOperator && !isEqualsOrClear && !this.pressedEquals) {
+      this.currentCalculation = this.currentCalculation.slice(
+        0,
+        this.currentCalculation.length - 1
+      );
     }
 
     switch (operator) {
       case 'C':
         this.currentCalculation = '';
         this.calculatorResult = 0;
-        this.firstOperation = true;
         this.pressedOperator = false;
-        this.clearOperator();
+        this.pressedNumber = false;
+        this.currentNumber = '';
+        this.arrayToCalculate = [];
+        this.pressedEquals = false;
         return;
-      case '/':
-        this.performOperation('/');
-        this.operationForEquals = '/';
-        break;
-      case '*':
-        this.performOperation('*');
-        this.operationForEquals = '*';
-        break;
-      case '-':
-        this.performOperation('-');
-        this.operationForEquals = '-';
-        break;
-      case '+':
-        this.performOperation('+');
-        this.operationForEquals = '+';
-        break;
       case '=':
-        try {
-          //replace so eval can handle comma as decimal
-          // this.currentCalculation = this.currentCalculation.replace(',', '.');
-          //cannot use eval for adding two decimals
-          // this.calculatorResult = eval(this.currentCalculation).toFixed(10);
-          this.performOperation(this.operationForEquals);
-          this.calculatorResult = this.resultSoFar;
-          this.currentCalculation = this.calculatorResult
-            .toString()
-            .replace('.', ',');
+        if (this.currentNumber !== '') {
+          this.arrayToCalculate.push(this.currentNumber);
+          this.currentNumber = '';
+        }
+        if (this.arrayToCalculate.length > 2) {
+          const calculationResult = this.calculateArray(this.arrayToCalculate);
+          this.calculatorResult = parseFloat(calculationResult);
+          this.arrayToCalculate = [calculationResult];
+          this.currentCalculation = calculationResult;
           this.pressedEquals = true;
-          this.pressedOperator = false;
-          this.firstOperation = true;
-          return;
-        } catch (error) {
-          alert('Invalid calculation');
         }
         return;
       default:
@@ -116,58 +100,21 @@ export class CalculatorService {
         break;
     }
 
-    this.pressedEquals = false;
-    this.pressedOperator = true;
+    this.currentOperator = operator;
+    if (this.currentNumber !== '') {
+      this.arrayToCalculate.push(this.currentNumber);
+      this.currentNumber = '';
+    }
     this.currentCalculation += operator;
-    this.numberToOperate = '';
+    this.currentNumber = '';
+    this.pressedOperator = true;
+    this.pressedNumber = false;
+    this.pressedEquals = false;
   }
 
   //added so layout doesnt break
   haveTooManyKeys(): boolean {
     return this.currentCalculation.length > 20;
-  }
-
-  //going this way to solve adding decimals
-  performOperation(operation: string) {
-    if (this.numberToOperate === '' && this.calculatorResult === 0) {
-      return;
-    }
-
-    //if this is the first operation, set the result to the number
-    if (this.firstOperation) {
-      this.resultSoFar =
-        this.calculatorResult != 0
-          ? this.calculatorResult
-          : parseFloat(this.numberToOperate.replace(',', '.'));
-      this.numberToOperate = '';
-      this.firstOperation = false;
-      return;
-    }
-
-    const number = parseFloat(this.numberToOperate.replace(',', '.'));
-    switch (operation) {
-      case '/':
-        this.resultSoFar /= number;
-        break;
-      case '*':
-        this.resultSoFar *= number;
-        break;
-      case '-':
-        this.resultSoFar -= number;
-        break;
-      case '+':
-        this.resultSoFar += number;
-        break;
-      default:
-        //do nothing
-        break;
-    }
-    this.numberToOperate = '';
-  }
-
-  clearOperator() {
-    this.numberToOperate = '';
-    this.resultSoFar = 0;
   }
 
   isOperatorCOrEquals(operator: string): boolean {
@@ -176,7 +123,85 @@ export class CalculatorService {
 
   handleEqualsPressed() {
     this.currentCalculation = '';
+    this.arrayToCalculate = [this.calculatorResult.toString()];
     this.calculatorResult = 0;
     this.pressedEquals = false;
+  }
+
+  calculateArray(calculation: Array<string>): string {
+    if (calculation.length === 1) {
+      return calculation[0];
+    }
+
+    const hasDivision = calculation.indexOf('/');
+    if (hasDivision !== -1) {
+      const result = this.calculateResult('/', hasDivision);
+      //if the result is undefined, it means we are dividing by 0 or something else went wrong
+      if (result === undefined) {
+        return "Can't calculate division";
+      }
+      calculation.splice(hasDivision - 1, 3, result.toString());
+      return this.calculateArray(calculation);
+    }
+
+    const hasMultiplication = calculation.indexOf('*');
+    if (hasMultiplication !== -1) {
+      const result = this.calculateResult('*', hasMultiplication);
+      if (result === undefined) {
+        return "Can't calculate multiplication";
+      }
+      calculation.splice(hasMultiplication - 1, 3, result.toString());
+      return this.calculateArray(calculation);
+    }
+
+    const hasSubtraction = calculation.indexOf('-');
+    if (hasSubtraction !== -1) {
+      const result = this.calculateResult('-', hasSubtraction);
+      if (result === undefined) {
+        return "Can't calculate subtraction";
+      }
+      calculation.splice(hasSubtraction - 1, 3, result.toString());
+      return this.calculateArray(calculation);
+    }
+
+    const hasAddition = calculation.indexOf('+');
+    if (hasAddition !== -1) {
+      const result = this.calculateResult('+', hasAddition);
+      if (result === undefined) {
+        return "Can't calculate addition";
+      }
+      calculation.splice(hasAddition - 1, 3, result.toString());
+      return this.calculateArray(calculation);
+    }
+
+    return "Can't calculate. Unknown.";
+  }
+
+  calculateResult(operation: string, index: number): number | undefined {
+    const number1 = this.arrayToCalculate[index - 1];
+    const number2 = this.arrayToCalculate[index + 1];
+    if (!number1) {
+      return;
+    }
+    if (!number2) {
+      return parseFloat(number1.replace(',', '.')); //if there is only one number, return it
+    }
+    const num1 = parseFloat(number1.replace(',', '.'));
+    const num2 = parseFloat(number2.replace(',', '.'));
+
+    switch (operation) {
+      case '/':
+        return num1 / num2;
+      case '*':
+        return num1 * num2;
+      case '-':
+        return num1 - num2;
+      case '+':
+        return num1 + num2;
+      default:
+        //do nothing
+        break;
+    }
+    return;
   }
 }
